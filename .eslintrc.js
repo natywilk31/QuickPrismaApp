14ef672af43deb5e886166f0cb3bd5a6aac2b6cc
Natalie Wilkins <natywilk31@gmail.com>
2025-04-20 21:11:02 -0600

    trying to get render towork

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  };
+  
